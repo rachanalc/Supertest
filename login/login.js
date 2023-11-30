f1af4cc913e381c906156af4db988fb05d8f2239
Bikash-Koirala <bikashkoirala25@gmail.com>
2023-11-30 22:19:42 +0545

    changes login and loginiij.js files

--- login/login.js
+++ login/login.js
@@ -1,14 +1,13 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const login =async () =>{
-    const response = await request ('localhost:3000')
+const login = async () => {
+  const response = await request("localhost:3000")
     .post("/api/v1/session")
     .send({
-        username:"superman",
-        password:"test1"
-
-
-    })
-    return response.headers['set-cookie']
-}
-module.exports={ login}
+      username: "superman",
+      password: "test1",
+    });
+  console.log("hello beautiful...");
+  return response.headers["set-cookie"];
+};
+module.exports = { login };
